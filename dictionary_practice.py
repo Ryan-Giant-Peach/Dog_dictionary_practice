fd3eafc8513889b4fcf3e9934f71711634c4838b
Ryan Caulfield <Ryancaulfield3103@gmail.com>
2022-01-06 19:00:58 +0000

    Add files via upload

---
+++ dictionary_practice.py
@@ -0,0 +1,28 @@
+
+
+dogs = {
+    "Goomba" : {
+        "age" : 3,
+        "species" : "Unknown",
+        "favourite food" : {"chicken", "street bread"
+        },
+    },
+    "Peanut" : {
+        "age" : 0,
+        "species" : "Cocker Spaniel",
+        "favourite food" : {"unknown", "chicken probably"
+        },
+    },
+}
+
+
+
+peanut_species = dogs["Peanut"]["species"]
+
+goomba_food = dogs["Goomba"]["favourite food"]
+
+print(goomba_food)
+print(peanut_species)
+  
+
+
